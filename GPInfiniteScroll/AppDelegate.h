86f46d69a11a30ddb29e04e010744fce59012e27
German Pereyra <german.f.pereyra@gmail.com>
2015-07-08 14:56:27 -0300

    Initial Commit

---
+++ GPInfiniteScroll/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GPInfiniteScroll
+//
+//  Created by German Pereyra on 7/8/15.
+//  Copyright (c) 2015 German Pereyra. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GPInfiniteScroll/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GPInfiniteScroll
+//
+//  Created by German Pereyra on 7/8/15.
+//  Copyright (c) 2015 German Pereyra. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
